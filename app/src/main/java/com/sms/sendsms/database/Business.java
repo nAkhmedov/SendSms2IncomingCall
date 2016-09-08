705f4e04735c8c61eb4383b377522ce594212ef4
nAkhmedov <akhmedovnavruzbek@gmail.com>
2016-09-08 13:19:35 +0500

    changes 2

--- app/src/main/java/com/sms/sendsms/database/Business.java
+++ app/src/main/java/com/sms/sendsms/database/Business.java
@@ -109,8 +109,6 @@ public class Business {
     private int test;
 
     // KEEP FIELDS - put your custom fields here
-
-    private String defaultColor = "#ffffff";
     // KEEP FIELDS END
 
     public Business() {
@@ -999,16 +997,6 @@ public class Business {
     }
 
     // KEEP METHODS - put your custom methods here
-    public void setCustomHeaderColor(String headerColor) {
-        if (headerColor.isEmpty()) {
-            headerColor = this.defaultColor;
-        }
-        this.headerColor = headerColor;
-    }
-
-    public void setCustomMaintextColor(String maintextColor) {
-        this.maintextColor = maintextColor;
-    }
     // KEEP METHODS END
 
 }
